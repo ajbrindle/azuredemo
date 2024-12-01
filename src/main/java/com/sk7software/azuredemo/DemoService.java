03faad664733b5966862d09fceff7120eeb925a2
ajbrindle <andrew.brindle@gmail.com>
2024-12-01 22:47:01 +0000

    Name service

--- src/main/java/com/sk7software/azuredemo/DemoService.java
+++ src/main/java/com/sk7software/azuredemo/DemoService.java
@@ -2,6 +2,7 @@ package com.sk7software.azuredemo;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class DemoService {
     @GetMapping("/demo")
     public String getDemo() {
         return "Hello World";
+    }
+
+    @GetMapping("/name/{name}")
+    public String getName(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
